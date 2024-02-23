d620aab95dfb5a281769ce70e8c5b959ceed6769
Zoltan Nagy <nazoma05@gmail.com>
2024-02-23 10:15:20 +0100

    small update on homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,17 +5,17 @@ import "./homeStyles.scss";
 const content = [
   {
     title: "About SAS Virtual",
-    img: "../public/home_img_1.jpg",
+    img: "/home_img_1.jpg",
     text: "Scandinavian Airlines Virtual is a virtual airline for flight simulation enthusiasts. We strive to provide a realistic and professional virtual aviation experience for our pilots. Our virtual airline is designed to simulate the operations of an airline, using real-world airline schedules, rules and procedures. We offer a variety of routes and aircraft, allowing our pilots to experience the thrill of flying in a realistic environment. Join us today and start your virtual aviation journey.",
   },
   {
     title: "Our Fleet",
-    img: "../public/home_img_2.jpg",
+    img: "/home_img_2.jpg",
     text: "At Scandinavian Airlines Virtual, we operate a diverse fleet of aircraft to serve our various routes. From short-haul regional jets to long-haul widebodies, our fleet is equipped to provide a comfortable and enjoyable flight experience.",
   },
   {
     title: "Our Destinations",
-    img: "../public/home_img_3.jpg",
+    img: "/home_img_3.jpg",
     text: "Scandinavian Airlines Virtual serves a wide range of destinations across the globe. Whether you're looking to explore the scenic landscapes of Scandinavia, the bustling cities of Europe, or the far reaches of Asia and North America, we have a flight for you.",
   },
 ];
