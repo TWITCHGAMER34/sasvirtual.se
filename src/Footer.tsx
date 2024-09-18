020987e438d60bc9adde7616ceed18f27801720f
TWITCHGAMER34 <nazoma05@gmail.com>
2024-09-18 12:13:51 +0200

    updated year and some names in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -7,14 +7,17 @@ import "./footer.scss";
  * @constructor
  */
 
+let date = new Date();
+let year = date.getFullYear();
+
 export function Footer() {
   return (
     <footer>
       <div className="footer-content">
         <div className="copyright">
-          <p>&copy; 2023 Scandinavian Airlines Virtual. All rights reserved.</p>
+          <p>&copy; {year} Scandinavian Airlines Virtual. All rights reserved.</p>
           <div className="made-by">
-            <p>Made by Nagy_Zoltán34 & Setilic UF | In cooperation with:</p>
+            <p>Made by Nagy_Zoltán34 & Setilic HB | In cooperation with:</p>
             <a href="https://discord.gg/zNpUf22Wxn" target="_blank">
               <img src="/zoltan.jpg" alt="Nagy_Zoltán34 Profile Picture" />
             </a>
